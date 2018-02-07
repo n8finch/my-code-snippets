eb725bd68a1ed26b231bd71e9d836eedcef24934
Nate Finch <finchproservices@gmail.com>
2018-02-06 21:13:32 -0600

    updates 🔥

--- wp-nav-menu/wp-nav-menu-customizations.php
+++ wp-nav-menu/wp-nav-menu-customizations.php
@@ -30,6 +30,28 @@ function add_search_box_to_menu( $items, $args ) {
     return $items;
 }
 
+add_filter('wp_nav_menu_items','chi_add_search_box_to_menu', 10, 2);
+function chi_add_search_box_to_menu( $items, $args ) {
+
+	$searchform = '<li><form class="searchform seach-form" role="search" method="get" action="http://whateversite.com/" _lpchecked="1">
+		<div class="search-table">
+			<div class="search-field">
+				<label class="screen-reader-text" for="searchform">Search for:</label>
+				<input id="searchform" type="text" value="" name="s" class="s" placeholder="Search ..." required="" aria-required="true" aria-label="Search ...">
+			</div>
+			<div class="search-button">
+				<input type="submit" class="searchsubmit" value="" alt="Search">
+			</div>
+		</div>
+	</form></li>';
+
+
+    if( $args->theme_location == 'top_navigation' )
+        return $items.$searchform;
+
+    return $items;
+}
+
 add_filter('wp_nav_menu_items','add_todaysdate_in_menu', 10, 2);
 function add_todaysdate_in_menu( $items, $args ) {
     if( $args->theme_location == 'primary')  {
